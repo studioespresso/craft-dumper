eec37026e4173ea272117603f3fca778248dad10
Jan Henckens <jan@studioespresso.co>
2017-10-08 15:11:26 +0200

    Added plugin!

---
+++ src/CraftDumper.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017 Studio Espresso
+ */
+
+namespace studioespresso\craftdumper;
+
+
+use Craft;
+use studioespresso\craftdumper\web\twig\DumperExtension;
+
+/**
+ * Plugin represents the Craft Dumper plugin.
+ *
+ * @author Studio Espresso <support@studioespresso.co>
+ * @license MIT License
+ * @link https://github.com/studioespresso/craft3-dumper
+ * @package Craft Dumper
+ * @since  1.0
+ */
+class CraftDumper extends \craft\base\Plugin
+{
+
+	public static $plugin;
+
+	public function init() {
+		self::$plugin = $this;
+		Craft::$app->view->twig->addExtension( new DumperExtension() );
+
+	}
+}
